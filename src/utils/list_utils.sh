6c3f95fab316f44ab8950799653e595819d5f4d6
rednammoc <rednammoc@gmx.de>
2014-02-24 02:13:34 +0100

    Added return 1 when file is missing. Fixed list-utils put index.

--- src/utils/list_utils.sh
+++ src/utils/list_utils.sh
@@ -1,14 +1,16 @@
 #!/bin/bash
 
 list_size () {
-	local LIST="$1"
-	echo $(wc -l < "${LIST}")
+	local list="$1"
+	! [ -f "${list}" ] && echo 0 && return 1
+	echo $(wc -l < "${list}")
 }
 
 list_enumerate () {
 	local list="$1"
 	local highlight="$2"
 	local index=1
+	! [ -f "${list}" ] && return 1
 	while read line
 	do
 		local color=''
@@ -26,16 +28,18 @@ list_enumerate () {
 list_enumerate_search () {
 	local list="$1"
 	local search_term="$2"
+	! [ -f "${list}" ] && return 1
 	# notice: this is not efficient.
 	list_enumerate "${list}" | grep -i "${search_term}" --color=always
 }
 
 list_get () {
-	local LIST="$1"
-    local INDEX="$2"
-	local LINE_COUNT=$(list_size "${LIST}")
-	if [[ "${INDEX}" -lt 1 ]] || [[ "${INDEX}" -gt "${LINE_COUNT}" ]] ; then return 1; fi
-    sed -n ${INDEX}p < "${LIST}"
+	local list="$1"
+    local index="$2"
+	! [ -f "${list}" ] && return 1
+	local line_count=$(list_size "${list}")
+	if [[ "${index}" -lt 1 ]] || [[ "${index}" -gt "${line_count}" ]] ; then return 1; fi
+    sed -n ${index}p < "${list}"
     return 0
 }
 
@@ -43,7 +47,7 @@ list_put () {
 	local list="$1"
 	local entry="$2"
 	local index="$3"
-	if [ -z "${index}" ]
+	if [ -z "${index}" ] || [ "${index}" -le "1" ] || [ "${index}" -gt $(list_size "${list}") ]
 	then
 		echo "${entry}" >> "${list}"
 	else
@@ -54,11 +58,13 @@ list_put () {
 list_delete () {
 	local list="$1"
 	local index="$2"
+	! [ -f "${list}" ] && return 1
 	sed -i "${index}d" "${list}"
 }
 
 list_clear () {
 	local list="$1"
+	! [ -f "${list}" ] && return 1
 	> "${list}"
 }
 
