4e6cd3761cb1a96f8b760f4eafc4750b4b290a84
rednammoc <rednammoc@gmx.de>
2014-03-15 13:17:40 +0100

    Added some more comments to list_utils_test.sh

--- tests/utils/list_utils_test.sh
+++ tests/utils/list_utils_test.sh
@@ -1,6 +1,5 @@
 #!/bin/bash
 APP_NAME="list_utils_test.sh"
-TEST="`readlink -e $0`"
 BASH_TEST_DIR=${BASH_SOURCE%/*}
 BASH_UTILS_DIR="${BASH_TEST_DIR}/../../src/utils"
 if [[ ! -d "$BASH_UTILS_DIR" ]]; then BASH_UTILS_DIR="$PWD"; fi
@@ -9,18 +8,15 @@ source "${BASH_UTILS_DIR}/core_utils.sh"
 require "assert_utils.sh"
 require "list_utils.sh"
 
+# Gather script-name and location
+script="`readlink -e $0`"
+
+# Setup and tear_down list for each test.
 list="/tmp/list_utils_test"
+function setup { touch "${list}"; }
+function tear_down { rm -rf "${list}"; }
 
-function setup
-{
-	touch "${list}"
-}
-
-function tear_down
-{
-	rm -rf "${list}"
-}
-
+# Tests
 function testListSize_WhenEmpty_ReturnZero
 {
 	assert "list_size \"${list}\"" "0"
@@ -32,4 +28,5 @@ function testListSize_WhenPutElementIntoEmptyList_ReturnOne
 	assert "list_size \"${list}\"" "1"
 }
 
-run_tests "${TEST}"
+# Automatically run tests.
+run_tests "${script}"
